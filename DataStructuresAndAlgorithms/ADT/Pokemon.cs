a12e1a6c8f33fb1cb47e11033f31f678d26a9bd3
noblelar <59696875+noblelar@users.noreply.github.com>
2024-03-04 22:31:03 +0000

    DS_start

---
+++ DataStructuresAndAlgorithms/ADT/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.ADT
+{
+    internal class Pokemon
+    {
+        private string Name {  get; set; }
+        private int Ext {  get; set; }
+
+        public bool IsPokemon() { return Name.Length > 0; }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace DataStructuresAndAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Stack ts = new Stack();
+        }
+    }
+}
