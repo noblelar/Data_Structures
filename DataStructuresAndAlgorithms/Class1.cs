4fc4228553c8099cfc1d1a7c5ad613125d098a9b
noblelar <59696875+noblelar@users.noreply.github.com>
2024-03-08 22:07:03 +0000

    LinkedListType

---
+++ DataStructuresAndAlgorithms/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public static class Class1
+    {
+        // Define some strings to add to the list
+        //string[] comedians = {"Leo Gallagher", "Rodney Dangerfield", "George Carlin", "Lenny Bruce", "Eddie Murphy", "Louie Anderson"};
+
+
+        // TODO: Create the list
+        // List<string> strList = new List<string>(10);
+
+
+        // TODO: Add some elements to the list from an existing array
+        // strList.AddRange(comedians);
+
+
+            // TODO: Add individual items
+            // strList.Add("Tina Fey");
+            // strList.Add("Amy Poehler");
+
+
+            // TODO: Examine the Count and Capacity
+            // Console.WriteLine("Count and Capacity: {0}, {1}", strList.Count, strList.Capacity);
+
+
+            // TODO: Enumerate the items in the list with a foreach loop
+            // PrintList(strList);
+
+
+            // TODO: Access any item by its index
+            // Console.WriteLine("Item value: {0}", strList[4]);
+
+
+            // TODO: Remove items from the list
+            // strList.RemoveAt(2);
+            // strList.Remove("Tina Fey");
+
+
+            // TODO: Sort the list content
+            // strList.Sort();
+            // PrintList(strList);
+
+
+            // ** Searching List Content **/
+
+            // TODO: Determine if a list contains a given item
+            // Console.WriteLine(strList.Contains("Ron Gallagher"));
+
+
+            // TODO: The Exists function gives us a more customizable way to search
+            // by using a predicate function to examine each element in the list
+            // bool found = strList.Exists(x => x.Length > 15);
+            // Console.WriteLine($"Item found: {found}");
+
+
+            // TODO: Use the Find function to search through items
+            // string item = "";
+            // item = strList.Find(match: x => x.StartsWith("L"));
+            // Console.WriteLine(item);
+
+
+            // TODO: The FindAll function can find multiple items
+            // List<string> itemList = strList.FindAll(match: x => x.StartsWith("L"));
+        // PrintList(itemList);
+
+        // TODO: Use TrueForAll to see if a given condition is true for all elements
+        // bool result = strList.TrueForAll(x => x.StartsWith("L"));
+        // Console.WriteLine($" Result: {result}");
+
+
+            // static void PrintList(List<string> theList)
+            // {
+                // foreach (string str in theList)
+                // {
+                   //  Console.WriteLine(str);
+                // }
+            // }
+
+
+        // Console.WriteLine("Hello, World!");
+
+        // Stack ts = new Stack();
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -6,84 +6,53 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main(string[] args)
         {
-            // Define some strings to add to the list
-            string[] comedians = {"Leo Gallagher", "Rodney Dangerfield",
-                     "George Carlin", "Lenny Bruce",
-                     "Eddie Murphy", "Louie Anderson"};
-            // TODO: Create the list
-            List<string> strList = new List<string>(10);
+            // Define some strings that represent song names
+            string[] songs = {"Shout", "Satisfaction", "Help!",
+                            "Stairway to Heaven", "Come Sail Away",
+                            "All Night Long", "Right Here Right Now"};
+
+            // TODO: Create a LinkedList that holds strings
+            LinkedList<string> myList = new LinkedList<string>(songs);
 
 
-            // TODO: Add some elements to the list from an existing array
-            strList.AddRange(comedians);
+
+            // TODO: Items can be added at the front or back of the list
+            myList.AddFirst("Africa");
+            myList.AddLast("The Twist");
 
 
-            // TODO: Add individual items
-            strList.Add("Tina Fey");
-            strList.Add("Amy Poehler");
+            // TODO: Like other collections, a LinkedList can be iterated
+            //foreach (string s in myList)
+            //{
+              //  Console.WriteLine(s);
+            //}
 
+            // TODO: First and Last properties return LinkedListNodes
+            LinkedListNode<string> first = myList.First;
+            LinkedListNode<string> last = myList.Last;
+            //Console.WriteLine(first.Value);
+            //Console.WriteLine(last.Value);
 
-            // TODO: Examine the Count and Capacity
-            Console.WriteLine("Count and Capacity: {0}, {1}", strList.Count, strList.Capacity);
+            // TODO: Items can be added or removed relative to an existing item
+            myList.AddAfter(first, "Here comes the sun");
+            myList.AddBefore(last, "Awesome one");
 
+            //foreach (string s in myList)
+            //{
+              //  Console.WriteLine(s);
+            //}
 
-            // TODO: Enumerate the items in the list with a foreach loop
-            // PrintList(strList);
+            // TODO: Search for items in the list with Contains and Find
+            Console.WriteLine(myList.Contains("Satisfaction"));
+            Console.WriteLine(myList.Find("Help"));
 
+            // TODO: The list can then be traversed with those properties 
+            Console.WriteLine(first.Next.Value);
+            Console.WriteLine(last.Previous.Value);
 
-            // TODO: Access any item by its index
-            Console.WriteLine("Item value: {0}", strList[4]);
-
-
-            // TODO: Remove items from the list
-            strList.RemoveAt(2);
-            strList.Remove("Tina Fey");
-
-
-            // TODO: Sort the list content
-            strList.Sort();
-            // PrintList(strList);
-
-
-            // ** Searching List Content **/
-
-            // TODO: Determine if a list contains a given item
-            Console.WriteLine(strList.Contains("Ron Gallagher"));
-
-
-            // TODO: The Exists function gives us a more customizable way to search
-            // by using a predicate function to examine each element in the list
-            bool found = strList.Exists(x => x.Length > 15);
-            Console.WriteLine($"Item found: {found}");
-
-
-            // TODO: Use the Find function to search through items
-            string item = "";
-            item = strList.Find(match: x => x.StartsWith("L"));
-            Console.WriteLine(item);
-
-
-            // TODO: The FindAll function can find multiple items
-            List<string> itemList = strList.FindAll(match: x => x.StartsWith("L"));
-            PrintList(itemList);
-
-            // TODO: Use TrueForAll to see if a given condition is true for all elements
-            bool result = strList.TrueForAll(x => x.StartsWith("L"));
-            Console.WriteLine($" Result: {result}");
-
-
-            static void PrintList(List<string> theList)
-            {
-                foreach (string str in theList)
-                {
-                    Console.WriteLine(str);
-                }
-            }
-
-
-            // Console.WriteLine("Hello, World!");
-
-            // Stack ts = new Stack();
         }
+
+
     }
+
 }
